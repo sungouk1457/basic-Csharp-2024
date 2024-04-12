277c94ff30e17468b35b2c24cd79f8fb3b4aa394
sungouk1457 <sungouk1457@naver.com>
2024-04-12 13:02:07 +0900

    C#2일차

---
+++ day02/cs02_basic_app/ex08_generics/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ex08_generics
+{
+    internal class Program
+    {
+        // 배열 복사 기능
+        static void CopyArray(int[] src, int[] dest)
+        {
+            for (int i = 0; i < src.Length; i++)
+                dest[i] = src[i];
+        }
+        static void CopyArray(float[] src, float[] dest)
+        {
+            for (int i = 0; i < src.Length; i++)
+                dest[i] = src[i];
+        }
+        static void Main(string[] args)
+        {
+            int[] array1 = { 10, 20, 30, 40, 50 };
+            int[] array2 = new int[array1.Length];
+
+            CopyArray(array1, array2);
+
+            Console.WriteLine(array2[4]);
+
+            float[] array3 = { 3.4f, 4.4f, 2.2f };
+            float[] array4 = new float[array3.Length];
+
+            CopyArray(array3, array4);
+
+            Console.WriteLine(array4[2]);
+        }
+    }
+}
